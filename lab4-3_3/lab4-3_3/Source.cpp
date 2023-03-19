31803af14b3272d81b02c5191217d0feaa6f715b
Fruity <fruityflunt@gmail.com>
2023-03-19 06:45:42 +0300

    3_3: lil fixes, new approach

--- lab4-3_3/lab4-3_3/Source.cpp
+++ lab4-3_3/lab4-3_3/Source.cpp
@@ -1,4 +1,5 @@
 ﻿//3.3.Распил бруса(7)
+//
 //  На пилораму привезли брус длиной L метров.Известны места, в которых необходимо сделать
 //  распилы.Стоимость одного распила равна длине распиливаемого бруса.Различный порядок
 //  распилов приводит к различным итоговым ценам.Например, брус длиной 10 метров, который
@@ -6,12 +7,15 @@
 //  распилить сначала на отметке 2, потом 4 и потом 7. Стоимость будет 10 + 8 + 6 = 24. А если сначала
 //  распилить на отметке 4, потом 2 и затем 7, то стоимость составит 10 + 4 + 6 = 20. Требуется написать
 //  программу, которая определяла бы минимальную стоимость распила.
+//
 //  Ввод из файла INPUT.TXT.В первой строке содержатся через пробел натуральные числа L
 //  и N(1 ≤ L ≤ 500, N &lt; L) – длина бруса и число разрезов.Вторая строка содержит N натуральных
 //  чисел C i(0 & lt; C i &lt; L) через пробел, задающих в строго возрастающем порядке места, в которых
 //  нужно сделать распилы.
+//
 //  Вывод в файл OUTPUT.TXT.В единственной строке вывести минимальную стоимость
 //  распилов.
+//
 //  Примеры
 //  Ввод 1 Ввод 2
 //  10 2	10 4
@@ -41,45 +45,6 @@ struct TablePosition
 	size_t x;
 	size_t y;
 };
-
-
-CostTable GetMinCostTable(CuttingPoints& cuttingPoints);
-
-int main()
-{
-	std::ifstream inputFile("input.txt");
-	if (!inputFile.is_open())
-	{
-		return 1;
-	}
-
-	int L;
-	int N;
-
-	inputFile >> L >> N;
-	CuttingPoints cuttingPoints;
-	cuttingPoints.push_back(MIN_CUTTING_POINT);
-
-	for (int i = 0; i < N; ++i)
-	{
-		int cuttingPoint;
-		inputFile >> cuttingPoint;
-		cuttingPoints.push_back(cuttingPoint);
-	}
-	cuttingPoints.push_back(L);
-
-	//for (int i = 0; i < cuttingPoints.size(); i++)
-	//{
-	//	std::cout << cuttingPoints[i] <<std::endl;
-	//}
-
-	CostTable costTable = GetMinCostTable(cuttingPoints);
-	std::ofstream outputFile("output.txt");
-	outputFile << costTable[INITIAL_ROW][costTable.size() - 1] << std::endl;
-	std::cout << costTable[INITIAL_ROW][costTable.size() - 1] << std::endl;
-
-	return 0;
-}
 
 
 
@@ -121,8 +86,8 @@ void PrintCostTable(CostTable& costTable)
 		}
 		std::cout << std::endl;
 	}
-		std::cout << "----------------------------------------------------------------" << std::endl;
-		std::cout << std::endl;
+	std::cout << "------------------------------------" << std::endl;
+	std::cout << std::endl;
 }
 
 void FillMinCostTable(CostTable& costTable, CuttingPoints& cuttingPoints)
@@ -161,4 +126,49 @@ CostTable GetMinCostTable(CuttingPoints& cuttingPoints)
 	FillMinCostTable(result, cuttingPoints);
 
 	return result;
+}
+
+void HandleInput(std::istream& inputStream, int& L, int& N, CuttingPoints& cuttingPoints)
+{
+	inputStream >> L >> N;
+	cuttingPoints.push_back(0);
+	for (int i = 0; i < N; i++)
+	{
+		int cuttingPoint;
+		inputStream >> cuttingPoint;
+		cuttingPoints.push_back(cuttingPoint);
+	}
+	cuttingPoints.push_back(L);
+}
+
+void HandleOutput(std::ostream& outputStream, int minCuttingPrice)
+{
+	outputStream << minCuttingPrice << std::endl;
+}
+
+int main()
+{
+	std::ifstream inputFile("input.txt");
+	if (!inputFile.is_open())
+	{
+		return 1;
+	}
+
+	int L;
+	int N;
+	CuttingPoints cuttingPoints;
+	HandleInput(inputFile, L, N, cuttingPoints);
+	
+
+	CostTable costTable = GetMinCostTable(cuttingPoints);
+	int minCuttingPrice = costTable[0][costTable.size() - 1];
+
+	std::ofstream outputFile("output.txt");
+	if (!outputFile.is_open())
+	{
+		return 1;
+	}
+	HandleOutput(std::cout, minCuttingPrice);
+
+	return 0;
 }
