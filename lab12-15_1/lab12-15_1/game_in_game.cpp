21209ec729e1a3289d9d55713c388712ddf59e30
Fruity <fruityflunt@gmail.com>
2023-04-25 07:42:26 +0300

    15_1: done

---
+++ lab12-15_1/lab12-15_1/game_in_game.cpp
@@ -0,0 +1,77 @@
+﻿//	15.1.Игра в игре(5)
+//
+//	Вася и Петя любят игры со спичками. Несколько дней они играли в следующую игру. Два игрока поочередно забирают из кучки, содержащей N спичек, от 1 до M спичек.
+//	Выигрывает игрок, после хода которого кучка спичек опустеет.Доведя до совершенства свое мастерство, они решили изменить правила.Первый игрок задает значение N.
+//	Потом второй игрок указывает величину M.Далее игроки поочередно берут спички, начиная с первого игрока.
+//	По жребию первым игроком оказался Вася.Для продления удовольствия Петя при любых дальнейших ответах Васи собирается выиграть так, чтобы партия состояла из наибольшего числа ходов.
+//	Укажите правильный ответ Пети.
+//
+//	Ввод из файла INPUT.TXT.В единственной строке содержится значение N(2 ≤ N ≤ 10^12).
+//
+//	Вывод в файл OUTPUT.TXT.В единственной строке выводится значение M(2 ≤ M ≤ N). Если у Пети нет выигрывающего хода, вывести 0.
+//
+//	Примеры
+//	Ввод 1          Ввод 2
+//	3               2
+//	Вывод 1         Вывод 2
+//	2               0
+//	
+//	Данилов Владимир, ПС-21. Visual Studio 2017.
+
+#include <fstream>
+#include <iostream>
+
+using LongNumber = unsigned long long;
+
+void HandleInput(std::istream& inputStream, LongNumber& N)
+{
+	inputStream >> N;
+}
+
+void HandleOutput(std::ostream& outputStream, LongNumber& M)
+{
+	outputStream << M;
+}
+
+LongNumber FindStepsToWin(LongNumber& N)
+{
+	if ((N - 1) > 1)
+	{
+		return N - 1;
+	}
+
+	for (LongNumber i = 3; i <= static_cast<LongNumber>(std::sqrt(N) + 1); i++)
+	{
+		if (N % i == 0)
+		{
+			return i - 1;
+		}
+	}
+
+	return 0;
+}
+
+
+
+int main()
+{
+	std::ifstream inputFile("input.txt");
+	if (!inputFile.is_open())
+	{
+		return 1;
+	}
+
+	LongNumber N;
+	HandleInput(inputFile, N);
+
+	LongNumber M = FindStepsToWin(N);
+
+	std::ofstream outputFile("output.txt");
+	if (!outputFile.is_open())
+	{
+		return 1;
+	}
+
+	HandleOutput(outputFile, M);
+	return 0;
+}
